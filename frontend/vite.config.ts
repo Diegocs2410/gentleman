f46b9d272fb2acbb2f4bb4e359e0a8ed73f85a2a
diego.armando@onespan.com <diego.armando@onespan.com>
2023-03-11 19:50:26 -0500

    adding tailwind and login page

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePluginFonts } from 'vite-plugin-fonts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    // ...
+    VitePluginFonts({
+      google: {
+        families: [
+          {
+            name: 'Montserrat',
+            styles: 'wght@400;700'
+          },
+          {
+            name: 'Playfair Display',
+            styles: 'wght@400;700'
+          }
+        ]
+      }
+    }),
+    // ...
+    react()
+  ]
 })
